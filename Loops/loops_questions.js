742f911f27675f9d7812e70849e29cacfb9a33ef
iamasadshah <ibnerafi76@gmail.com>
2023-12-20 11:51:42 +0500

    4th question added

--- Loops/loops_questions.js
+++ Loops/loops_questions.js
@@ -1,5 +1,4 @@
-
-  /* QUESTION #1
+/* QUESTION #1
 1. Create a variable to be used as the max value for the number guessing game. 
 2. Generate a random number for the solution using Math.random() and Math.floor(). You will also need to add 1 so that the value is returned as 1-[whatever the set max value is]. You can log this value to the console for development to see the value as you create the game, then when the game is complete you can comment out this console output.
 3. Create a variable that will be used for tracking whether the answer is correct or not and set it to a default Boolean value of false. We can update it to be true if the user guess is a match.  
@@ -13,24 +12,23 @@ let randomNumber = Math.floor(Math.random() * maxValue + 1);
 
 let checkAnswer = false;
 
-while(!checkAnswer){
-    let userNumber = prompt("Enter a number between 1 and 5");
-    userNumber = parseInt(userNumber);
-   
-    // console.log("Your Guess is correct")
-    break;
-    if(userNumber === randomNumber){
-        checkAnswer = true;
-        console.log("You Guess the correct Number")
-    }else if(userNumber > randomNumber){
-        console.log("Too High! Try Again..")
-    }else{
-        console.log("Too Low! try Again")
-    }
+while (!checkAnswer) {
+  let userNumber = prompt("Enter a number between 1 and 5");
+  userNumber = parseInt(userNumber);
+
+  // console.log("Your Guess is correct")
+  break;
+  if (userNumber === randomNumber) {
+    checkAnswer = true;
+    console.log("You Guess the correct Number");
+  } else if (userNumber > randomNumber) {
+    console.log("Too High! Try Again..");
+  } else {
+    console.log("Too Low! try Again");
+  }
 }
 
-
-  /*QUESTION 2
+/*QUESTION 2
 In this exercise, we will create a basic counter that will increase a dynamic variable by a consistent step value, up to an upper limit. 
 1. Set the starting counter to 0 
 2. Create a variable, step, to increase your counter by 
@@ -38,12 +36,12 @@ In this exercise, we will create a basic counter that will increase a dynamic va
 4. Continue to loop until the counter is equal to 100 or more than 100
 */
 
-  let counter = 0;
-  let step = 5;
-  do {
-    console.log("Counter: ", counter);
-    counter += step;
-  } while (counter < 100);
+let counter = 0;
+let step = 5;
+do {
+  console.log("Counter: ", counter);
+  counter += step;
+} while (counter < 100);
 
 /* QUESTION 3
 1. Setup a blank array, myWork. 
@@ -55,13 +53,40 @@ In this exercise, we will create a basic counter that will increase a dynamic va
  
  */
 
- let myWork =[];
- for(let list = 0; list < 10; list++){
-   let status = list % 2 === 0 ? false : true;
-   let lesson = {
-     name : "Lesson " + list,
-     status : status
-   };
-   myWork.push(lesson);
- }
-console.log(myWork)
+let myWork = [];
+for (let list = 0; list < 10; list++) {
+  let status = list % 2 === 0 ? false : true;
+  let lesson = {
+    name: "Lesson " + list,
+    status: status,
+  };
+  myWork.push(lesson);
+}
+console.log(myWork);
+
+/*QUESTION 4
+ 1. To create a table generator, first create an empty array, myTable, to hold your table data. 
+ 2. Set variable values for the number of rows and columns. This will allow us to dynamically control how many rows and columns we want within the table. Separating the values from the main code helps make updates to the dimensions easier. 
+ 3. Set up a counter variable with an initial value of 0. The counter will be used to set the content and count the values of the cells within the table. 
+ 4. Create a for loop with conditions to set the number of iterations, and to construct each row of the table. Within it, set up a new temporary array (tempTable) to hold each row of data. The columns will be nested within the rows, generating each cell needed for the column.   
+ 5. Nest a second loop within the first to count the columns. Columns are run within the row loop so that we have a uniform number of columns within the table. 
+6. Increment the main counter each iteration of the inner loop, so that we track a master count of each one of the cells and how many cells are created. 
+7. Push the counter values to the temporary array, tempTable. Since the array is a nested array representing a table, the values of the counter can also be used to illustrate the cell values next to each other in the table. Although these are separate arrays representing new rows, the value of the counter will help illustrate the overall sequence of cells in the final table. 
+8. Push the temporary array to the main table. As each iteration builds a new row of array items, this will continue to build the main table in the array.  
+9. Output into the console with console.table(myTable). This will show you a visual representation of the table structure. 
+ */
+
+let myTable = [];
+let numRows = 5;
+let numColm = 4;
+let counter = 0;
+
+for (i = 0; 1 < numRows; i++) {
+  let tempTable = [];
+  for (j = 0; j < numColm; j++) {
+    counter++;
+    tempTable.push(counter);
+  }
+  myTable.push(tempTable);
+}
+console.log(myTable);
